9ac156d1eb5f0a44b8ca029aa86575077615cd4d
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-12-10 10:54:20 -0800

    Add no-cors, make build production

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const model = require('./model.js');
+const cors = require('cors');
 
 const app = express();
 const port = 3003;
 
+app.use(cors());
 app.use('/', express.static(path.join(__dirname, '../public')));
 app.use('/:id', express.static(path.join(__dirname, '../public')));
 
